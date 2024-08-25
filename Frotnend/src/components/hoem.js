4a7aad36134803c0e1341fa7b8bd279afd02524d
Mahesh Chopade <65208771+mahesh2-lab@users.noreply.github.com>
2024-08-25 23:18:16 +0530

    Update hoem.js

--- Frotnend/src/components/hoem.js
+++ Frotnend/src/components/hoem.js
@@ -2,7 +2,7 @@
 import L from 'leaflet';
 
 export const customIcon = L.icon({
-  iconUrl: "icon.png",
+  iconUrl: "https://w7.pngwing.com/pngs/457/630/png-transparent-location-logo-location-computer-icons-symbol-location-miscellaneous-angle-heart.png",
   iconSize: [32, 32], // Size of the icon
   iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
   popupAnchor: [0, -32], // Point from which the popup should open relative to the iconAnchor
